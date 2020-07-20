8003f99cb13f8f973e8f0ed99b172d25ea2bac3b
shih_ps63 <r02945009@ntu.edu.tw>
2020-07-21 03:00:23 +0800

    stroop UI modified

--- stroop.py
+++ stroop.py
@@ -3,6 +3,8 @@ import tkinter.font as font
 import random 
 import time
 
+TRIAL_TIMES = 20
+
 class stroop():
     def __init__(self, window):
         self.word_list = ['紅', '黃', '綠', '藍']
@@ -33,11 +35,12 @@ class stroop():
         self.canvas.create_text(400, 120, font=("Times New Roman", 28), text='判斷文字顏色')
         self.canvas.create_text(400, 200, font=("Times New Roman", 20), text='開始後請依照上方出現的文字顏色作答')
         self.canvas.create_text(400, 250, font=("Times New Roman", 20), text='作答時按鍵盤上對應答案的數字')
-        self.canvas.create_text(400, 300, font=("Times New Roman", 20), text='舉例:出現紅色的""藍""就要選紅,所以要按下1')
-        self.canvas.create_text(400, 350, font=("Times New Roman", 20), text='準備好就可以開始！')
+        self.canvas.create_text(400, 300, font=("Times New Roman", 20), text='紅  黃  綠  藍')
+        self.canvas.create_text(400, 350, font=("Times New Roman", 20), text='1   2   3   4')
+        self.canvas.create_text(400, 400, font=("Times New Roman", 20), text='準備好就可以開始！')
         self.canvas.pack()
         self.btn = tk.Button(self.window, text="開始遊戲", width=15, height=4)
-        self.btn.place(x=330, y=400)
+        self.btn.place(x=330, y=450)
         self.btn.config(command=self.run)
 
     def red_trigger(self):
@@ -52,7 +55,7 @@ class stroop():
 
         self.check_trial()
         self.new_question()
-        
+
     def yellow_trigger(self):
         if self.correct_ans == 'yellow':
             self.result_label.configure(text='正確!', font=self.fontsize)
@@ -110,12 +113,12 @@ class stroop():
         else:
             self.is_congruent = False
         self.correct_ans = self.color_list[c]
-        self.description_label.configure(text=self.word_list[w], font=self.fontsize,fg=self.correct_ans)
+        self.description_label.configure(text=self.word_list[w], font=font.Font(size=36),fg=self.correct_ans, bg='black')
         self.spawn_time = time.time()
         self.trial += 1
         
     def check_trial(self):
-        if self.trial == 40:
+        if self.trial == TRIAL_TIMES:
             self.instruction_label.destroy()
             self.description_label.destroy()
             self.result_label.destroy()
@@ -138,8 +141,8 @@ class stroop():
             
             self.canvas.create_text(400, 200, font=("Times New Roman", 24), text='在有答對的情況下')
             self.canvas.create_text(400, 250, font=("Times New Roman", 24), text='文字跟顏色一致時你的判斷時間 : {:.3f}'.format(congruent_avg))
-            self.canvas.create_text(400, 300, font=("Times New Roman", 24), text='文字跟顏色一致時你的判斷時間 : {:.3f}'.format(incongruent_avg))
-            self.canvas.create_text(400, 350, font=("Times New Roman", 24), text='史楚普效應為後者時間減去前者時間 : '.format(incongruent_avg-congruent_avg))
+            self.canvas.create_text(400, 300, font=("Times New Roman", 24), text='文字跟顏色不一致時你的判斷時間 : {:.3f}'.format(incongruent_avg))
+            self.canvas.create_text(400, 350, font=("Times New Roman", 24), text='史楚普效應為後者時間減去前者時間 : {:.3f}'.format(incongruent_avg-congruent_avg))
             self.canvas.create_text(400, 500, font=("Times New Roman", 24), text='按下Esc鍵繼續...')
             self.canvas.pack()
             self.window.bind("<Escape>", lambda event:self.refresh())
@@ -153,41 +156,42 @@ class stroop():
         self.instruction_label.pack()
         
         self.btn.destroy()
-        self.description_label = tk.Label(self.window)
+        self.description_label = tk.Label(self.window, width=6, height=3)
         self.description_label.pack()
 
         self.result_label = tk.Label(self.window)
         self.result_label.pack()
         
-        self.label_frame = tk.Frame(self.window)
-        self.label_frame.pack(side=tk.TOP)
-        
-        self.label1 = tk.Label(self.label_frame, text='1', font=self.fontsize, width=6, height=3)
-        self.label1.pack(side = tk.LEFT, fill="x", expand=True)
-
-        self.label2 = tk.Label(self.label_frame, text='2', font=self.fontsize, width=6, height=3)
-        self.label2.pack(side = tk.LEFT, fill="x", expand=True)
-
-        self.label3 = tk.Label(self.label_frame, text='3', font=self.fontsize, width=6, height=3)
-        self.label3.pack(side = tk.LEFT,fill="x", expand=True)
-
-        self.label4 = tk.Label(self.label_frame, text='4', font=self.fontsize, width=6, height=3)
-        self.label4.pack(side = tk.LEFT,fill="x", expand=True)
 
         self.ans_frame = tk.Frame(self.window)
         self.ans_frame.pack(side=tk.TOP)
         
-        self.red_label = tk.Label(self.ans_frame, text='紅', font=self.fontsize, width=6, height=3)
+        self.red_label = tk.Label(self.ans_frame, text='紅', font=self.fontsize, width=6, height=2)
         self.red_label.pack(side = tk.LEFT,fill="x", expand=True)
 
-        self.yellow_label = tk.Label(self.ans_frame, text='黃', font=self.fontsize, width=6, height=3)
+        self.yellow_label = tk.Label(self.ans_frame, text='黃', font=self.fontsize, width=6, height=2)
         self.yellow_label.pack(side = tk.LEFT,fill="x", expand=True)
 
-        self.green_label = tk.Label(self.ans_frame, text='綠', font=self.fontsize, width=6, height=3)
+        self.green_label = tk.Label(self.ans_frame, text='綠', font=self.fontsize, width=6, height=2)
         self.green_label.pack(side = tk.LEFT,fill="x", expand=True)
 
-        self.blue_label = tk.Label(self.ans_frame, text='藍', font=self.fontsize, width=6, height=3)
+        self.blue_label = tk.Label(self.ans_frame, text='藍', font=self.fontsize, width=6, height=2)
         self.blue_label.pack(side = tk.LEFT,fill="x", expand=True)
+        
+        self.label_frame = tk.Frame(self.window)
+        self.label_frame.pack(side=tk.TOP)
+        
+        self.label1 = tk.Label(self.label_frame, text='1', font=self.fontsize, width=6, height=2)
+        self.label1.pack(side = tk.LEFT, fill="x", expand=True)
+
+        self.label2 = tk.Label(self.label_frame, text='2', font=self.fontsize, width=6, height=2)
+        self.label2.pack(side = tk.LEFT, fill="x", expand=True)
+
+        self.label3 = tk.Label(self.label_frame, text='3', font=self.fontsize, width=6, height=2)
+        self.label3.pack(side = tk.LEFT,fill="x", expand=True)
+
+        self.label4 = tk.Label(self.label_frame, text='4', font=self.fontsize, width=6, height=2)
+        self.label4.pack(side = tk.LEFT,fill="x", expand=True)
         
         self.key_frame = tk.Frame(self.window)
         self.key_frame.bind("<KeyPress>", self.key_press)
