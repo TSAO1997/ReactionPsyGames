bac17b66b735b998acec63fc914d4d337f269a36
TSAO1997 <ws421111@gmail.com>
2020-07-29 03:11:01 +0800

    new result added and output file added
    Signed-off-by: TSAO1997 <ws421111@gmail.com>

--- gonogo.py
+++ gonogo.py
@@ -5,13 +5,14 @@ import numpy as np
 import cv2
 import tkinter as tk
 import time
-from  tkinter import ttk
+import csv
+from tkinter import ttk
 from PIL import ImageTk, Image
 
 class go_nogo_Game(object):
     def __init__(self, window):
         self.window = window
-        self.canvas = tk.Canvas(window, bg='green', height=600, width=800)
+        self.canvas = tk.Canvas(window, bg='lightgreen', height=600, width=800)
         self.start_time = 0
         self.end_time = 0
         self.zone_size = 150
@@ -20,10 +21,11 @@ class go_nogo_Game(object):
         self.strike = 1
         self.ball = 0
         self.error = 0
+        self.fp = 0
+        self.fn = 0
         self.press = False
         self.btn = None
-        self.time_step = 3000 # 3000(ms)
-        self.run_step = 10
+        self.run_step = 100
         self.curr_step = 1
         self.record_time = []
         self.respond_time = []
@@ -38,8 +40,8 @@ class go_nogo_Game(object):
     def tutorial(self):
         #self.btn.destroy()
         self.canvas.create_text(400, 120, font=("Times New Roman", 28), text='判斷好壞球')
-        self.canvas.create_text(400, 200, font=("Times New Roman", 20), text='以下會有十顆球，若為好球請按空白鍵')
-        self.canvas.create_text(400, 250, font=("Times New Roman", 20), text='十顆球後，將會顯示所有球的反應時間')
+        self.canvas.create_text(400, 200, font=("Times New Roman", 20), text='以下會有一百顆球，若為好球請按空白鍵')
+        self.canvas.create_text(400, 250, font=("Times New Roman", 20), text='一百顆球後，將會顯示所有球的反應時間')
         self.canvas.create_text(400, 300, font=("Times New Roman", 20), text='請加油！')
         self.canvas.pack()
         self.btn = tk.Button(self.window, text="開始遊戲", width=15, height=4)
@@ -52,23 +54,25 @@ class go_nogo_Game(object):
         self.canvas.delete("all")
         self.canvas.configure(bg='lightgreen')
         self.canvas.create_rectangle(400-self.zone_size, 300-self.zone_size, 400+self.zone_size, 300+self.zone_size, tags='zone')
+        '''
         self.canvas.create_line(250, 250, 550, 250, fill="black", tags='zone')
         self.canvas.create_line(250, 350, 550, 350, fill="black", tags='zone')
         self.canvas.create_line(350, 150, 350, 450, fill="black", tags='zone')
         self.canvas.create_line(450, 150, 450, 450, fill="black", tags='zone')
+        '''
         self.canvas.pack()
 
     def create_circle(self):
         # zone position : [x:250-550 , y:150-450]
         self.press = False
-        oval_x = np.random.rand(1)[0] * 400 + 200 # 200-600
-        oval_y = np.random.rand(1)[0] * 400 + 100 # 100-500
+        oval_x = np.random.rand(1)[0] * 400 + 200 # 200 - 600
+        oval_y = np.random.rand(1)[0] * 400 + 100 # 100 - 500
         oval_size = 10
         ball_pos = (abs(oval_x - (self.zone_x - self.zone_size)) < 10) or (abs(oval_x - (self.zone_x + self.zone_size)) < 10) or \
             (abs(oval_y - (self.zone_y - self.zone_size)) < 10) or (abs(oval_y - (self.zone_y - self.zone_size)) < 10)
         while ball_pos:
-            oval_x = np.random.rand(1)[0] * 400 + 200 # 200-600
-            oval_y = np.random.rand(1)[0] * 400 + 100
+            oval_x = np.random.rand(1)[0] * 400 + 200 # 200 - 600
+            oval_y = np.random.rand(1)[0] * 400 + 100 # 100 - 500
             ball_pos = (abs(oval_x - (self.zone_x - self.zone_size)) < 10) or (abs(oval_x - (self.zone_x + self.zone_size)) < 10) or \
                 (abs(oval_y - (self.zone_y - self.zone_size)) < 10) or (abs(oval_y - (self.zone_y - self.zone_size)) < 10)
         self.canvas.create_oval(oval_x - oval_size, oval_y - oval_size, oval_x + oval_size, oval_y + oval_size, fill='red', outline = "red", tags='circle') 
@@ -85,7 +89,7 @@ class go_nogo_Game(object):
             self.end_time = time.time()
             self.record_time.append(self.end_time - self.start_time)
             if ball_status == self.strike:
-                self.error += 1
+                self.fn += 1
         if self.curr_step < self.run_step:
             self.curr_step += 1
             self.create_circle()
@@ -98,7 +102,7 @@ class go_nogo_Game(object):
             self.record_time.append(self.end_time - self.start_time)
             self.press = True
             if ball_status == self.ball:
-                self.error += 1
+                self.fp += 1
         self.canvas.delete('circle')
         self.canvas.update()
         if ball_status == self.strike:
@@ -116,11 +120,11 @@ class go_nogo_Game(object):
     def show_result(self):
         self.canvas.delete('zone')
         # Respond time
-        response_time_text = '反應時間 : ' + str(round(sum(self.respond_time)/len(self.respond_time), 3)) + ' 秒'
         if len(self.respond_time) > 0:
-            respond_time = self.canvas.create_text(400, 120, font=("Purisa", 24), text=response_time_text)
+            response_time_text = '反應時間 : ' + str(round(sum(self.respond_time)/len(self.respond_time), 3)) + ' 秒'
+            respond_time = self.canvas.create_text(400, 100, font=("Purisa", 24), text=response_time_text)
         else:
-            respond_time = self.canvas.create_text(400, 120, font=("Purisa", 24), text='反應時間 : 0 秒')      
+            respond_time = self.canvas.create_text(400, 100, font=("Purisa", 24), text='反應時間 : 0 秒')      
         
         # Respond table
         style = ttk.Style(self.window)
@@ -137,35 +141,47 @@ class go_nogo_Game(object):
                 self.table.insert('', 'end', values=[str(i+1), str(round(self.record_time[i], 3))], tags=('odd'))
             else:
                 self.table.insert('', 'end', values=[str(i+1), str(round(self.record_time[i], 3))], tags=('even'))
-        #table.tag_configure('odd', background='orange')
-        #table.tag_configure('even', background='orange')
-        self.table.place(x=200, y=180)
+        self.table.place(x=200, y=150)
 
         # Accuracy
-        self.canvas.create_text(400, 500, font=("Purisa", 24), text='準確率: {} %'.format(100 * (self.run_step - self.error) / self.run_step))
-        self.canvas.create_text(400, 550, font=("Purisa", 24), text='按下Esc鍵繼續...')
+        self.canvas.create_text(400, 500, font=("Purisa", 24), text='準確率: {} %'.format(100 * (self.run_step - (self.fp + self.fn)) / self.run_step))
+        self.canvas.create_text(400, 550, font=("Purisa", 12), text='按下Esc鍵繼續...')
         self.window.bind("<Escape>", lambda event:self.refresh())
+
+        # save the result
+        self.btn = tk.Button(self.window, text="儲存結果", width=10, height=3)
+        self.btn.place(x=350, y=400)
+        self.btn.config(command=self.save_result)
+    
+    def save_result(self):
+        f = open('result.txt', 'w')
+        f.write('編號  反應時間(秒)\n')
+        for i in range(len(self.record_time)):
+            result = '{}    {}\n'.format(i+1, round(self.record_time[i], 3))
+            f.write(result)
+            if i == len(self.record_time) - 1:
+                f.write('\n')     
+        if len(self.respond_time) == 0:
+            f.write('整體反應時間 : 0 秒\n')
+        else:
+            f.write('整體平均反應時間 : {} 秒\n'.format(round(sum(self.respond_time)/len(self.respond_time), 3)))
+        f.write('整體錯誤次數 : {} 次\n'.format(self.fn + self.fp))
+        f.write('好球沒按到次數 : {} 次\n'.format(self.fn))
+        f.write('壞球按錯次數 : {} 次\n'.format(self.fp))
 
     def main(self):
         self.create_zone()
         self.canvas.update() 
         self.canvas.after(1000)
         self.create_circle()
-        '''
-        for i in range(self.run_step):
-            self.create_circle()
-            self.canvas.update()
-            self.delete_circle()
-            self.window.after(1500 + i * self.time_step, self.create_circle)
-        self.window.after(1500 + self.run_step * self.time_step, self.print_result)
-        '''
+        
 if __name__ == '__main__':
     window=tk.Tk()
     window.title('Go nogo')
     window.geometry('800x600')
     window.configure(background='white')
     canvas = tk.Canvas(window, bg='white', height=600, width=800)
-    run_step = 10
-    game = go_nogo_Game(window, canvas, run_step)
+    run_step = 100
+    game = go_nogo_Game(window, canvas)
     game.menu_picture()
     window.mainloop()
